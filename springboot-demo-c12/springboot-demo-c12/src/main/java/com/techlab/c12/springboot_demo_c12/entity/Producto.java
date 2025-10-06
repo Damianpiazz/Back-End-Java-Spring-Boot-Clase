534eee583ef392c4b6473f993d5942487fb2a06d
Damianpiazz <damipiazza333@gmail.com>
2025-10-06 12:07:48 -0300

    docs: Agregue el README.md que contiene la teoria y la consigna de la (clase 12)

---
+++ springboot-demo-c12/springboot-demo-c12/src/main/java/com/techlab/c12/springboot_demo_c12/entity/Producto.java
@@ -0,0 +1,4 @@
+package com.techlab.c12.springboot_demo_c12.entity;
+
+public class Producto {
+}
---
+++ springboot-demo-c12/springboot-demo-c12/src/main/java/com/techlab/c12/springboot_demo_c12/service/ProductoService.java
@@ -0,0 +1,4 @@
+package com.techlab.c12.springboot_demo_c12.service;
+
+public class ProductoService {
+}
