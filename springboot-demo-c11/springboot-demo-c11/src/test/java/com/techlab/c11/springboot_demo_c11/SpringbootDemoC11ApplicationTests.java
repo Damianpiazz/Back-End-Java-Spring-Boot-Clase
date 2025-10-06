d59ff825bbc5fcf1fc39de1f2195054f06885725
Damianpiazz <damipiazza333@gmail.com>
2025-10-06 02:33:57 -0300

    feat: Agregue el ejercicio practico de la (clase 11)

---
+++ springboot-demo-c11/springboot-demo-c11/src/test/java/com/techlab/c11/springboot_demo_c11/SpringbootDemoC11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.c11.springboot_demo_c11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoC11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
