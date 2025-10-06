ca2153cb24ab9bdc5f86e598ad385fa8bb2a4275
Damianpiazz <damipiazza333@gmail.com>
2025-10-06 12:08:36 -0300

    feat: Agregue la resolucion del ejercicio de la (clase 12)

--- springboot-demo-c12/springboot-demo-c12/src/main/java/com/techlab/c12/springboot_demo_c12/controller/ProductoController.java
+++ springboot-demo-c12/springboot-demo-c12/src/main/java/com/techlab/c12/springboot_demo_c12/controller/ProductoController.java
@@ -1,4 +1,28 @@
 package com.techlab.c12.springboot_demo_c12.controller;
 
+import com.techlab.c12.springboot_demo_c12.service.ProductoService;
+import com.techlab.c12.springboot_demo_c12.entity.Producto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
 public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> listarProductos() {
+        return productoService.listarTodos();
+    }
+
+    @PostMapping
+    public Producto crearProducto(@RequestBody Producto producto) {
+        return productoService.guardar(producto);
+    }
 }
--- springboot-demo-c12/springboot-demo-c12/src/main/java/com/techlab/c12/springboot_demo_c12/repository/ProductoRepository.java
+++ springboot-demo-c12/springboot-demo-c12/src/main/java/com/techlab/c12/springboot_demo_c12/repository/ProductoRepository.java
@@ -1,4 +1,4 @@
-package com.techlab.c12.springboot_demo_c12.service;
+package com.techlab.c12.springboot_demo_c12.repository;
 
 import com.techlab.c12.springboot_demo_c12.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
